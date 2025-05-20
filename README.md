288c2b5035d89983de9bb3edb69bc34b54d93cd9
Ilya Lityagin <godplsgivememiku@gmail.com>
2025-05-20 12:13:10 +0300

    Version 1.3.0: - C O L O U R F U L logging - now able to get ssh key from nexus in raw or jar formats (see readme)

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Plugin syncs proto files from repository
 - Plugin allows to push to remote with `commitChangeRequest` task, changes are only tracked on proto files in build/resources/main/proto-sync directory
 - Plugin resolves imports, so you don't have to include all supporting and shared proto files
 - Plugin allows to set rules to how to fetch proto files on proto repository, for example to divide production files from development using rules as in example below. Rules support kind of pattern matching: if rule starts with * plugin will think that all branches that end with it are pass and if rules ends with * branch will pass if it's name start with said prefix
-
+- Plugin allows to switch to ssh connection. HTTPS link will be automatically converted to ssh format. All additional info contained in `sshKeyWhereabouts` property, see example below.
 ## Installation
 In plugins section of your `build.gradle(.kts)` add following:
 ```kotlin
@@ -17,6 +17,8 @@ In plugins section of your `build.gradle(.kts)` add following:
 1. Configure plugin in your `build.gradle(.kts)` file:
 
 ```kotlin
+import ru.miku.proto.sync.plugin.SshConfiguration
+
 protoSync {
     repository = "URL to repository with your proto files"
     //values in list are equal to proto files names, case is ignored
@@ -24,11 +26,23 @@ protoSync {
     //tells how deep to clone your repo
     depth = 3
     //rules are relation of current branch of project to branch in proto repo
-    rules = mapOf("master" to "main")
-    //default branch to go if there's no rule
+    //any branches starting with 'release/' will look at main branch as well as master branch
+    //any branches ending with 'test' will look at test branch
+    rules = mapOf("master" to "main", "release/*" to "main", "*test" to "test")
+    //all other branches will look at dev branch
     defaultBranch = "dev"
     //optional parameter which allows to add prefix to branches created with commitChangeRequest task
     autoBranchPrefix = "test-prefix"
+    //optional parameter, contains coordinates of artifact containing key. following example for jar containing id_ed25519 file in root dir
+    keyStorage = mapOf<SshConfiguration, String>(
+        SshConfiguration.FILE_TYPE to "jar", //supports jar and raw, zip in plans
+        SshConfiguration.FILE_NAME_IN_ARCHIVE to "id_ed25519", //can be omitted if download is raw
+        SshConfiguration.NEXUS_URL to "https://nexus-host/repository/maven-releases/ru/miku/key-storage/1.0.0/key-storage-1.0.0.jar",
+        //currently supports only basic auth, if no props provided, will try to access unauthorized
+        //will try to get these props from system props and then from env
+        SshConfiguration.USER_ENV to "NEXUS_USER",
+        SshConfiguration.PASSWORD_ENV to "NEXUS_PASSWORD",
+    )
 }
 ```
 
--- src/main/groovy/ru/miku/proto/sync/plugin/ProtoSyncPlugin.groovy
+++ src/main/groovy/ru/miku/proto/sync/plugin/ProtoSyncPlugin.groovy
@@ -14,6 +14,7 @@ import org.gradle.api.tasks.TaskAction
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.Instant
+import java.util.jar.JarFile
 
 class ProtoSyncPlugin implements Plugin<Project> {
     void apply(Project project) {
@@ -30,6 +31,7 @@ class ProtoSyncPlugin implements Plugin<Project> {
                 depth = config.depth ?: 3
                 rules = config.rules
                 defaultBranch = config.defaultBranch
+                sshKeyWhereabouts = config.sshKeyWhereabouts
             }
             project.tasks.register('commitChangeRequest', ChangeRequestTask) {
                 group = 'proto'
@@ -37,6 +39,7 @@ class ProtoSyncPlugin implements Plugin<Project> {
                 repository = config.repository
                 output = calculatedOutput
                 prefix = config.autoBranchPrefix
+                sshKeyWhereabouts = config.sshKeyWhereabouts
             }
             project.tasks.register('cleanAfterMyself', CleanupTask) {
                 group = 'proto'
@@ -69,6 +72,15 @@ class ProtoSyncPlugin implements Plugin<Project> {
     }
 }
 
+enum SshConfiguration {
+    NEXUS_URL,
+    FILE_TYPE,
+    FILE_NAME_IN_ARCHIVE,
+    USER_ENV,
+    PASSWORD_ENV,
+    KEY_PATH
+}
+
 class Configuration {
     String repository
     List<String> services
@@ -76,6 +88,65 @@ class Configuration {
     String defaultBranch
     Map<String, String> rules
     String autoBranchPrefix
+    Map<SshConfiguration, String> sshKeyWhereabouts
+}
+
+class CloneCommand {
+    String repository
+    private String repositorySSH
+    String repositoryPath
+    Integer depth
+    String branch
+    private Mode mode
+
+    CloneCommand(
+            String repository,
+            String repositoryPath,
+            Integer depth,
+            String branch
+    ) {
+        this.repository = repository
+        this.repositorySSH = convertToSSH(repository)
+        this.repositoryPath = repositoryPath
+        this.depth = depth
+        this.branch = branch
+        if (branch == null) mode = Mode.NO_BRANCH else mode = Mode.WITH_BRANCH
+    }
+
+    List<String> cmd() {
+        if (mode == Mode.WITH_BRANCH) {
+            ['git', 'clone', "--depth=$depth", '--branch', branch, '--single-branch', repository, repositoryPath]
+        } else {
+            ['git', 'clone', "--depth=$depth", repository, repositoryPath]
+        }
+    }
+
+    List<String> cmdSSH() {
+        String depthCMD = "--depth=$depth".toString()
+        if (mode == Mode.WITH_BRANCH) {
+            ['git', 'clone', depthCMD, '--branch', branch, '--single-branch', repositorySSH, repositoryPath]
+        } else {
+            ['git', 'clone', depthCMD, repositorySSH, repositoryPath]
+        }
+    }
+
+    private enum Mode {
+        WITH_BRANCH,
+        NO_BRANCH
+    }
+
+    private String convertToSSH(String repository) {
+        if (repository.startsWith("git")) return repository
+        def pattern = ~/https?:\/\/(.*?)(?:\/|:)(.*?)(?:\.git)?$/
+        def matcher = repository.toLowerCase() =~ pattern
+        if (matcher.matches()) {
+            def domain = matcher[0][1]
+            def repoPath = matcher[0][2]
+            return "git@${domain}:${repoPath}.git"
+        } else {
+            throw new IllegalArgumentException("${Utils.error()} Not a valid HTTP(S) Git URL: $repository")
+        }
+    }
 }
 
 class SyncProtoTask extends DefaultTask {
@@ -92,8 +163,14 @@ class SyncProtoTask extends DefaultTask {
     @Input
     Integer depth
 
+    @Input
+    Map<SshConfiguration, String> sshKeyWhereabouts
+
     @Internal
     File temp = project.file("${project.buildDir}/temp")
+
+    @Internal
+    File keyTemp = project.file("${project.buildDir}/ssh")
 
     @Input
     String defaultBranch
@@ -103,24 +180,29 @@ class SyncProtoTask extends DefaultTask {
 
     @TaskAction
     void execute() {
-        if (defaultBranch == null) throw new IllegalStateException("No default branch provided, please provide in configuration under defaultBranch parameter!")
-        if (rules == null || rules.isEmpty()) println "[INFO] No branching rules provided, always using default branch!"
-        def currentBranch = System.getProperty("BRANCH")
-        if (currentBranch == null) throw new IllegalStateException("No branch name provided via properties. Example: -DBRANCH=NAME gradle build ... ")
+        if (defaultBranch == null) throw new IllegalStateException("[proto-sync::FATAL] No default branch provided, please provide in configuration under defaultBranch parameter!")
+        if (rules == null || rules.isEmpty()) println Utils.invalidConf("No branching rules provided, always using default branch!")
+        temp.parentFile.mkdirs()
+        keyTemp.parentFile.mkdirs()
+        def currentBranch = System.getProperty("BRANCH") ?: System.getenv("BRANCH")
+        if (Utils.isStringNotOK(currentBranch)) println Utils.invalidConf("No project branch name provided via properties. Example: -DBRANCH=NAME gradle ... . Using default ($defaultBranch).")
+        def branch
         String rulesBranch
         if ((rulesBranch = findInRules(rules, currentBranch)) == null) {
-            println "[INFO] Using default branch: $defaultBranch to clone..."
-            Utils.runProcess(['git', 'clone', "--depth=$depth", '--branch', defaultBranch, '--single-branch', repository, temp.absolutePath])
+            println "${Utils.info()} Using default branch: $defaultBranch to clone..."
+            branch = defaultBranch
         } else {
-            println "[INFO] Cloning branch: $rulesBranch..."
-            Utils.runProcess(['git', 'clone', "--depth=$depth", '--branch', rulesBranch, '--single-branch', repository, temp.absolutePath])
+            println "${Utils.info()} Cloning branch: $rulesBranch..."
+            branch = rulesBranch
         }
-        println "[INFO] Successfully cloned repository."
-        println "[INFO] Now filtering only files requested..."
+        sshKeyWhereabouts.put(SshConfiguration.KEY_PATH, keyTemp.absolutePath)
+        Utils.clone(repository, branch, depth, temp.absolutePath, sshKeyWhereabouts)
+        println Utils.success("Successfully cloned repository.")
+        println "${Utils.info()} Now filtering only files requested..."
         List<Path> alreadyFound = []
         def filesToLook = requested.collect { (it + ".proto").toUpperCase() }.unique()
-        if (!filesToLook.isEmpty()) println "[INFO] Initially looking for ${filesToLook.toString()}"
-        else println "[INFO] No services in configuration, filtering skipped."
+        if (!filesToLook.isEmpty()) println "${Utils.info()} Initially looking for ${filesToLook.toString()}"
+        else println Utils.invalidConf("No services in configuration, filtering skipped.")
         while (!filesToLook.isEmpty()) {
             def additionallyFound = []
             def found = []
@@ -148,24 +230,24 @@ class SyncProtoTask extends DefaultTask {
                         } else {
                             protoDir.mkdirs()
                         }
-                        println "[INFO] File ${it.name} created."
+                        println "${Utils.info()} File ${it.name} created."
                         target.write(it.text)
-                        println "[INFO] Protofile ${it.name} is ready to use."
+                        println "${Utils.info()} Protofile ${it.name} is ready to use."
                         found << name
                     }
                 }
             }
             filesToLook = filesToLook.findAll { !found.contains(it) }
-            if (!filesToLook.isEmpty()) println "[INFO] Following services were NOT found: ${filesToLook.toString()}"
+            if (!filesToLook.isEmpty()) println "${Utils.error()} Following services were NOT found: ${filesToLook.toString()}"
             filesToLook.clear()
             filesToLook.addAll(additionallyFound)
-            if (!filesToLook.isEmpty()) println "[INFO] Found additional dependency protos: ${filesToLook.toString()}"
+            if (!filesToLook.isEmpty()) println "${Utils.info()} Found additional dependency protos: ${filesToLook.toString()}"
         }
-        println "[SUCCESS] Done filtering cloned files."
+        println Utils.success("Done filtering cloned files.")
     }
 
     private String findInRules(Map<String, String> rules, String branch) {
-        if (rules == null || rules.isEmpty()) return null
+        if (rules == null || rules.isEmpty() || branch == null) return null
         if (rules.containsKey(branch)) return rules.get(branch)
         return rules.findAll {
             it.key.contains("*")
@@ -181,14 +263,169 @@ class SyncProtoTask extends DefaultTask {
 }
 
 class Utils {
+
+    static boolean isStringNotOK(String patient) {
+        patient == null || patient.isEmpty() || patient.isBlank() || patient == 'null'
+    }
+
+    static String info() {
+        "${yellow()}[proto-sync::INFO]${reset()}"
+    }
+
+    static String invalidConf(String msg) {
+        "${blue()}[proto-sync::POSSIBLE INVALID CONFIGURATION] $msg${reset()}"
+    }
+    
+    static String error() {
+        "${red()}[proto-sync::ERROR]${reset()}"
+    }
+    
+    static String success(String msg) {
+        "${green()}[proto-sync::SUCCESS] $msg${reset()}"
+    }
+    
+    private static String green() { '\u001B[32m' }
+
+    private static String red() { '\u001B[31m' }
+
+    private static String yellow() { '\u001B[33m' }
+
+    private static String blue() { '\u001B[34m' }
+
+    private static String cyan() { '\u001B[36m' }
+
+    static String reset() { '\u001B[0m' }
+
     static void runProcess(List<String> command, File runAt = null) {
+        def proc = runAt ? command.execute(null, runAt) : command.execute()
+        runProcess(proc)
+    }
+
+    static void runProcess(Process proc) {
         def _ = new StringBuffer()
         def out = new StringBuffer()
-        def proc = runAt ? command.execute(null, runAt) : command.execute()
         proc.consumeProcessOutput(_, out)
         def exitCode = proc.waitFor()
-        if (out.length() > 0) println "[INFO] git log: \n\t> ${def buf = out.toString(); buf.substring(0, buf.length() - 1).replace("\n", "\n\t> ")}"
-        if (exitCode != 0) throw new IllegalStateException("Non zero exit code from git process, aborting...")
+        if (out.length() > 0) println "${cyan()}[proto-sync::EXTERNAL] git:\n\t> ${def buf = out.toString(); buf.substring(0, buf.length() - 1).replace("\n", "\n\t> ")}${reset()}"
+        if (exitCode != 0) throw new IllegalStateException("[proto-sync::FATAL] Non zero exit code from git process, aborting...")
+    }
+
+    static void runProcess(ProcessBuilder pb, File runAt = null) {
+        if (runAt) pb.directory(runAt)
+        runProcess(pb.start())
+    }
+
+    static void clone(
+            String repository,
+            String branch = null,
+            Integer depth,
+            String repositoryPath,
+            Map<SshConfiguration, String> sshKeyWhereabouts,
+            File runAt = null
+    ) {
+        def command = new CloneCommand(repository, repositoryPath, depth, branch)
+        try {
+            println "${info()} Cloning by HTTPS..."
+            runProcess(command.cmd(), runAt)
+        } catch (Exception ignored) {
+            if (!containsNotNull(sshKeyWhereabouts, SshConfiguration.NEXUS_URL)) {
+                println invalidConf("No key URL provided, not trying ssh, aborting...")
+                throw ignored
+            }
+            println "${info()} Cloning by HTTPS failed, trying ssh..."
+            def key = getKeyFromNexus(sshKeyWhereabouts)
+            def keyPath = sanitizePath(key)
+            sshKeyWhereabouts.put(SshConfiguration.KEY_PATH, keyPath)
+            def pb = processBuilder(command.cmdSSH(),
+                ['GIT_SSH_COMMAND' : "ssh -i $keyPath".toString()]
+            )
+            runProcess(pb, runAt)
+        }
+    }
+
+    static ProcessBuilder processBuilder(List<String> cmd, Map<String, String> env) {
+        def pb = new ProcessBuilder(cmd)
+        pb.environment().putAll(env)
+        pb
+    }
+
+    static String sanitizePath(File key) {
+        def path = key.absolutePath
+        if (path.contains("\\")) path = path.replaceAll("\\\\", "/")
+        path
+    }
+
+    static boolean containsNotNull(Map<SshConfiguration, String> map, SshConfiguration... key) {
+        key.every {
+            map.get(it) != null
+        }
+    }
+
+    static String safeGetProperty(Map<SshConfiguration, String> props, SshConfiguration key) {
+        def value = props.get(key)
+        if (isStringNotOK(value)) throw new IllegalStateException("[proto-sync::FATAL] Invalid value in property ${key.name()}: ${value}")
+        value
+    }
+
+    static File getKeyFromNexus(Map<SshConfiguration, String> sshKeyWhereabouts) {
+        def keyFileName = safeGetProperty(sshKeyWhereabouts, SshConfiguration.FILE_NAME_IN_ARCHIVE)
+        FileType mode
+        switch (safeGetProperty(sshKeyWhereabouts, SshConfiguration.FILE_TYPE).toLowerCase()) {
+            case 'jar': mode = FileType.JAR
+                break
+            default: mode = FileType.RAW
+        }
+        def link = safeGetProperty(sshKeyWhereabouts, SshConfiguration.NEXUS_URL)
+        def connection = new URL(link).openConnection()
+        if (containsNotNull(sshKeyWhereabouts, SshConfiguration.USER_ENV, SshConfiguration.PASSWORD_ENV)) {
+            def userKey = safeGetProperty(sshKeyWhereabouts, SshConfiguration.USER_ENV)
+            def passwordKey = safeGetProperty(sshKeyWhereabouts, SshConfiguration.PASSWORD_ENV)
+            def user = System.getProperty(userKey) ?: System.getenv(userKey)
+            def password = System.getProperty(passwordKey) ?: System.getenv(passwordKey)
+            if (user == null) throw new IllegalStateException("[proto-sync::FATAL] No property exists on provided property: $userKey.")
+            if (password == null) throw new IllegalStateException("[proto-sync::FATAL] No property exists on provided property: $passwordKey.")
+            connection.setRequestProperty("Authorization", "Basic " + "$user:$password".bytes.encodeBase64().toString())
+        } else println invalidConf("No auth credentials provided, going in unauthorized.")
+        def path = safeGetProperty(sshKeyWhereabouts, SshConfiguration.KEY_PATH)
+        def download = Paths.get(path).resolve("download${mode.ext}").normalize().toFile()
+        println success("Successfully downloaded ${download.name}.")
+        download.parentFile.mkdirs()
+        download.withOutputStream { out ->
+            connection.inputStream.withStream { input ->
+                out << input
+            }
+        }
+        def output = Paths.get(path).resolve("ssh-key").normalize().toFile()
+        output.parentFile.mkdirs()
+        output.createNewFile()
+        switch (mode) {
+            case FileType.JAR:
+                def jar = new JarFile(download)
+                def key = jar.getJarEntry(keyFileName)
+                def is = jar.getInputStream(key)
+                def os = new FileOutputStream(output)
+                byte[] buffer = new byte[4096]
+                int len
+                while ((len = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, len)
+                }
+                jar.close()
+                break
+            default: output.write(download.text)
+        }
+        println success("Successfully processed key file, stored as ${Paths.get(path).parent.parent.relativize(output.toPath())}.")
+        return output
+    }
+
+    private enum FileType {
+        JAR('.jar'),
+        RAW('');
+
+        final String ext
+
+        FileType(String ext) {
+            this.ext = ext
+        }
     }
 }
 
@@ -197,12 +434,21 @@ class CleanupTask extends DefaultTask {
     @Internal
     File temp = project.file("${project.buildDir}/temp")
 
+    @Internal
+    File key = project.file("${project.buildDir}/ssh")
+
     @TaskAction
     void execute() {
         def git = new File(temp, ".git")
         if (git.exists()) git.deleteDir()
-        temp.deleteDir()
-        println "cleaned!"
+        if (temp.exists()) {
+            project.delete(temp)
+            println "${Utils.info()} Temp directory cleaned."
+        }
+        if (key.exists()) {
+            project.delete(key)
+            println "${Utils.info()} ssh directory cleaned."
+        }
     }
 }
 
@@ -217,15 +463,20 @@ class ChangeRequestTask extends DefaultTask {
     @Input
     String prefix
 
+    @Input
+    Map<SshConfiguration, String> sshKeyWhereabouts
+
     @TaskAction
     void execute() {
         def source = new File(output)
         def tempRepo = new File(source, "temp-repo")
+        File keyTemp = project.file("${project.buildDir}/key")
+        sshKeyWhereabouts.put(SshConfiguration.KEY_PATH, keyTemp.absolutePath)
         def git = new File(tempRepo, ".git")
         if (git.exists()) git.deleteDir()
         if (tempRepo.exists()) tempRepo.deleteDir()
-        def branchName = "${prefix ? "$prefix-" : ""}${System.getProperty("user.name") ?: "UNKNOWN_USERNAME"}-at-${Instant.now().toEpochMilli()}"
-        Utils.runProcess(["git", "clone", "--depth=1", repository, tempRepo.absolutePath])
+        def branchName = "${prefix ? "$prefix-" : ""}${System.getProperty("user.name") ?: "UNKNOWN_USERNAME"}-at-${Instant.now().toEpochMilli()}".toString()
+        Utils.clone(repository, null, 1, tempRepo.absolutePath, sshKeyWhereabouts)
         Utils.runProcess(["git", "checkout", "-b", branchName], tempRepo)
         source.eachFileRecurse { file ->
             if (!file.isFile()) return
@@ -236,9 +487,14 @@ class ChangeRequestTask extends DefaultTask {
         }
         Utils.runProcess(["git", "add", "."], tempRepo)
         Utils.runProcess(["git", "commit", "-m", branchName], tempRepo)
-        Utils.runProcess(["git", "push", "--set-upstream", "origin", branchName], tempRepo)
+        def cmd = ["git", "push", "--set-upstream", "origin", branchName]
+        def pb = Utils.processBuilder(
+                cmd,
+                ['GIT_SSH_COMMAND' : "ssh -i ${sshKeyWhereabouts.get(SshConfiguration.KEY_PATH)}".toString()]
+        )
+        Utils.runProcess(pb, tempRepo)
         if (git.exists()) git.deleteDir()
         if (tempRepo.exists()) tempRepo.deleteDir()
-        println "Successfully pushed branch $branchName to origin."
+        println Utils.success("Successfully pushed branch $branchName to origin.")
     }
 }
